55f8a6597ea8978549640c37439b6671765cf91d
Vlad Kisel <eencircled@gmail.com>
2016-09-13 18:58:10 +0200

    Javadoc link fix

--- joiner-core/src/main/java/cz/encircled/joiner/query/join/JoinGraphRegistry.java
+++ joiner-core/src/main/java/cz/encircled/joiner/query/join/JoinGraphRegistry.java
@@ -4,7 +4,7 @@ import java.util.Collection;
 import java.util.List;
 
 /**
- * JoinGraph allows to predefine a set of joins for a specific class that can be added to a query using {@link cz.encircled.joiner.query.Q#joinGraphs(String...)}
+ * JoinGraph allows to predefine a set of joins for a specific class that can be added to a query using {@link cz.encircled.joiner.query.JoinerQuery#joinGraphs(String...)}
  *
  * @author Vlad on 15-Aug-16.
  */
