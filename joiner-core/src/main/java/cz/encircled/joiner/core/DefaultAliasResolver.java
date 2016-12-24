cab16e6890a4a86105fe6345cd45811bd33933cc
Vlad Kisel <eencircled@gmail.com>
2016-12-24 12:12:11 +0100

    Alias cache key fix

--- joiner-core/src/main/java/cz/encircled/joiner/core/DefaultAliasResolver.java
+++ joiner-core/src/main/java/cz/encircled/joiner/core/DefaultAliasResolver.java
@@ -51,8 +51,7 @@ public class DefaultAliasResolver implements AliasResolver {
     }
 
     private Path<?> findPathOnParent(Path<?> parent, Class<?> targetType, JoinDescription joinDescription) {
-        // TODO more efficient cache key
-        String cacheKey = parent.getClass().getName() + parent.toString() + targetType.getSimpleName() + joinDescription.getOriginalAlias().toString();
+        String cacheKey = joinDescription.getOriginalAlias().toString() + parent.toString() + parent.getClass().getSimpleName() + targetType.getSimpleName();
         if (aliasCache.containsKey(cacheKey)) {
             return aliasCache.get(cacheKey);
         }
@@ -62,6 +61,12 @@ public class DefaultAliasResolver implements AliasResolver {
         return result;
     }
 
+    /**
+     * @param parent          parent join or "from" element
+     * @param targetType      type of joined attribute
+     * @param joinDescription join description
+     * @return path on parent Q class
+     */
     protected Path<?> doFindPathOnParent(Path<?> parent, Class<?> targetType, JoinDescription joinDescription) {
         List<Path<?>> candidatePaths = new ArrayList<>();
 
@@ -70,17 +75,7 @@ public class DefaultAliasResolver implements AliasResolver {
         }
 
         if (candidatePaths.isEmpty()) {
-            for (Class child : ReflectionUtils.getSubclasses(parent.getType(), entityManager)) {
-                try {
-                    Class clazz = Class.forName(child.getPackage().getName() + ".Q" + child.getSimpleName());
-                    Object childInstance = ReflectionUtils.instantiate(clazz, (String) parent.getMetadata().getElement());
-                    for (Field field : clazz.getFields()) {
-                        testAliasCandidate(targetType, candidatePaths, getField(field, childInstance));
-                    }
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
+            findPathOnParentSubclasses(parent, targetType, candidatePaths);
         }
 
         if (candidatePaths.isEmpty()) {
@@ -96,8 +91,28 @@ public class DefaultAliasResolver implements AliasResolver {
                     return candidatePath;
                 }
             }
-            // TODO add candidates to exception
             throw new JoinerException("Join with ambiguous alias : " + joinDescription + ". Multiple mappings found: " + candidatePaths);
+        }
+    }
+
+    /**
+     * Try to find attribute on a subclass of parent (for instance in test domain when looking for <code>Key</code> on <code>User</code>)
+     *
+     * @param parent         parent join or "from" element
+     * @param targetType     type of joined attribute
+     * @param candidatePaths container for found paths
+     */
+    private void findPathOnParentSubclasses(Path<?> parent, Class<?> targetType, List<Path<?>> candidatePaths) {
+        for (Class child : ReflectionUtils.getSubclasses(parent.getType(), entityManager)) {
+            try {
+                Class clazz = Class.forName(child.getPackage().getName() + ".Q" + child.getSimpleName());
+                Object childInstance = ReflectionUtils.instantiate(clazz, (String) parent.getMetadata().getElement());
+                for (Field field : clazz.getFields()) {
+                    testAliasCandidate(targetType, candidatePaths, getField(field, childInstance));
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
