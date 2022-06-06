5666c48bbd71ae7da4135c5f3bbfe66bdc229455
Vladislav Kisel <eencircled@gmail.com>
2022-06-06 15:44:48 +0200

    Fix eclipse tests

--- joiner-eclipse/src/test/java/cz/encircled/joiner/eclipse/AbstractEclipseTest.java
+++ joiner-eclipse/src/test/java/cz/encircled/joiner/eclipse/AbstractEclipseTest.java
@@ -9,15 +9,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.core.env.Profiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -31,6 +32,7 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
 @ContextConfiguration(classes = {TestConfig.class})
 @Transactional
 @TestExecutionListeners(listeners = {TestDataListener.class, DependencyInjectionTestExecutionListener.class, TransactionalTestExecutionListener.class})
+@TestPropertySource(properties = "orm=eclipse")
 public abstract class AbstractEclipseTest extends TestWithLogging {
 
     @Autowired
@@ -59,11 +61,11 @@ public abstract class AbstractEclipseTest extends TestWithLogging {
     }
 
     protected boolean isEclipse() {
-        return environment.acceptsProfiles(Profiles.of("eclipse"));
+        return true;//environment.acceptsProfiles(Profiles.of("eclipse"));
     }
 
     protected boolean isLoaded(Object entity, String attribute) {
-        return isLoaded(entity, attribute);
+        return Persistence.getPersistenceUtil().isLoaded(entity, attribute);
     }
 
 }
