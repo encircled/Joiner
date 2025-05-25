45954f1bc2d11193c9cfde9eda8640d0cd40927f
Vladislav Kisel <vladislav.kisel1@gmail.com>
2025-05-25 15:53:03 +0200

    Testing in progress

--- joiner-ksp/src/main/kotlin/cz/encircled/joiner/ksp/QuerydslProcessor.kt
+++ joiner-ksp/src/main/kotlin/cz/encircled/joiner/ksp/QuerydslProcessor.kt
@@ -9,25 +9,28 @@ import com.google.devtools.ksp.validate
  * This processor replicates the functionality of the Querydsl apt-maven-plugin.
  */
 class QuerydslProcessor(
-    private val codeGenerator: CodeGenerator,
-    private val logger: KSPLogger,
-    private val options: Map<String, String>
+    private val codeGenerator: CodeGenerator? = null,
+    private val logger: KSPLogger? = null,
+    private val options: Map<String, String> = mapOf()
 ) : SymbolProcessor {
 
+    fun Resolver.getSymbolsWithAnnotations(vararg annotations: String) =
+        annotations.flatMap { getSymbolsWithAnnotation(it) }
+
     override fun process(resolver: Resolver): List<KSAnnotated> {
-        logger.info("Starting Querydsl processor")
-        // Find all classes annotated with @Entity
-        val entitySymbols = resolver.getSymbolsWithAnnotation("jakarta.persistence.Entity")
-            .filterIsInstance<KSClassDeclaration>()
-            .filter { it.validate() }
-            .toList()
+        logger?.info("Starting Querydsl processor2")
+
+        // Find all classes annotated with @Entity\@MappedSuperclass
+        val entitySymbols =
+            resolver.getSymbolsWithAnnotations("jakarta.persistence.Entity", "jakarta.persistence.MappedSuperclass")
+                .filterIsInstance<KSClassDeclaration>()
+                .filter { it.validate() }
 
         if (entitySymbols.isEmpty()) {
-            logger.info("No jakarta @Entity classes found")
+            logger?.info("No jakarta entity classes found")
             return emptyList()
         }
 
-        // Process each entity
         entitySymbols.forEach { entityClass ->
             processEntity(entityClass)
         }
@@ -36,26 +39,27 @@ class QuerydslProcessor(
         return entitySymbols.filterNot { it.validate() }
     }
 
-    private fun processEntity(entityClass: KSClassDeclaration) {
+    fun processEntity(entityClass: KSClassDeclaration): String {
         val packageName = entityClass.packageName.asString()
         val className = entityClass.simpleName.asString()
         val qClassName = "Q$className"
 
         // Generate the metamodel class
-        val fileSpec = codeGenerator.createNewFile(
+        val fileSpec = codeGenerator?.createNewFile(
             dependencies = Dependencies(false, entityClass.containingFile!!),
             packageName = packageName,
             fileName = qClassName,
             extensionName = "java"
         )
 
+        val content = generateMetamodelClass(packageName, className, qClassName, entityClass)
         fileSpec.use { outputStream ->
             // Generate the metamodel class content
-            val content = generateMetamodelClass(packageName, className, qClassName, entityClass)
-            outputStream.write(content.toByteArray())
+            outputStream?.write(content.toByteArray())
         }
 
-        logger.info("Generated Querydsl metamodel class: $packageName.$qClassName")
+        logger?.info("Generated Querydsl metamodel class: $packageName.$qClassName")
+        return content
     }
 
     private fun generateMetamodelClass(
@@ -64,134 +68,154 @@ class QuerydslProcessor(
         qClassName: String,
         entityClass: KSClassDeclaration
     ): String {
-        // Build the metamodel class content
-        val sb = StringBuilder()
+        val ctx = ClassCtx()
+        ctx.append(
+            """
+        package $packageName;
+    
+        import static com.querydsl.core.types.PathMetadataFactory.*;
+        import com.querydsl.core.types.dsl.*;
+        import com.querydsl.core.types.PathMetadata;
+        import javax.annotation.processing.Generated;
+        import com.querydsl.core.types.Path;
+        import com.querydsl.core.types.dsl.PathInits;
+    
+        /**
+         * $qClassName is a Querydsl query type for $className
+         */
+        @Generated("cz.encircled.joiner.ksp.QuerydslProcessor")
+        public class $qClassName extends EntityPathBase<$className> {
 
-        // Package declaration
-        sb.append("package $packageName;\n\n")
+            private static final long serialVersionUID = ${className.hashCode()}L;
+            private static final PathInits INITS = PathInits.DIRECT2;
 
-        // Imports
-        sb.append("import static com.querydsl.core.types.PathMetadataFactory.*;\n\n")
-        sb.append("import com.querydsl.core.types.dsl.*;\n\n")
-        sb.append("import com.querydsl.core.types.PathMetadata;\n")
-        sb.append("import javax.annotation.processing.Generated;\n")
-        sb.append("import com.querydsl.core.types.Path;\n")
-        sb.append("import com.querydsl.core.types.dsl.PathInits;\n\n")
-
-        // Class declaration
-        sb.append("/**\n")
-        sb.append(" * $qClassName is a Querydsl query type for $className\n")
-        sb.append(" */\n")
-        sb.append("@Generated(\"cz.encircled.joiner.ksp.QuerydslProcessor\")\n")
-        sb.append("public class $qClassName extends EntityPathBase<$className> {\n\n")
-
-        // Serial version UID
-        sb.append("    private static final long serialVersionUID = ${className.hashCode()}L;\n\n")
-
-        // Path inits
-        sb.append("    private static final PathInits INITS = PathInits.DIRECT2;\n\n")
-
-        // Static instance
-        sb.append("    public static final $qClassName ${className.decapitalize()} = new $qClassName(\"${className.decapitalize()}\");\n\n")
+    """.trimIndent()
+        )
 
         // Process superclass if any
         val superClass = entityClass.superTypes.firstOrNull()?.resolve()?.declaration as? KSClassDeclaration
         if (superClass != null && superClass.qualifiedName?.asString() != "java.lang.Object" && superClass.qualifiedName?.asString() != "kotlin.Any") {
             val superClassName = superClass.simpleName.asString()
             val qSuperClassName = "Q$superClassName"
-            sb.append("    public final $qSuperClassName _super = new $qSuperClassName(this);\n\n")
+            ctx.append("    public final $qSuperClassName _super = new $qSuperClassName(this);\n")
         }
 
         // Process properties
         entityClass.getAllProperties().forEach { property ->
-            processProperty(sb, property, entityClass)
+            processProperty(ctx, property)
         }
 
+        // Default static accessor
+        val staticAccessor = getStaticAccessor(ctx, className)
+        ctx.append("    public static final $qClassName $staticAccessor = new $qClassName(\"$staticAccessor\");")
+
         // Constructors
-        generateConstructors(sb, className, qClassName)
+        generateConstructors(ctx, className, qClassName)
 
         // Close class
-        sb.append("}\n")
+        ctx.append("}\n")
 
-        return sb.toString()
+        return ctx.toString()
     }
 
-    private fun processProperty(sb: StringBuilder, property: KSPropertyDeclaration, entityClass: KSClassDeclaration) {
+    private fun getStaticAccessor(ctx: ClassCtx, className: String): String {
+        val candidate = className.decapitalize()
+        if (ctx.fieldNames.contains(candidate)) {
+            var i = 1
+            while (ctx.fieldNames.contains(candidate + i)) {
+                i++
+            }
+            return candidate + i
+        }
+        return candidate
+    }
+
+    private fun processProperty(ctx: ClassCtx, property: KSPropertyDeclaration) {
         val propertyName = property.simpleName.asString()
         val propertyType = property.type.resolve()
         val propertyTypeDeclaration = propertyType.declaration
+
+        logger?.info("Processing property $propertyName of type ${propertyType.declaration.qualifiedName?.asString()}")
 
         // Skip transient properties
         if (property.annotations.any { it.shortName.asString() == "Transient" }) {
             return
         }
 
+        ctx.fieldNames.add(propertyName)
+
         // Handle different property types
         when {
-            // Basic types
-            propertyType.isBasicType() -> {
-                val pathType = getPathTypeForBasicType(propertyType)
-                sb.append("    public final $pathType $propertyName = createSimple(\"$propertyName\", ${propertyType.declaration.qualifiedName?.asString()}.class);\n\n")
-            }
             // Collection types
             propertyType.isCollectionType() -> {
+                logger?.info("Property $propertyName is a collection type")
                 val elementType = propertyType.arguments.firstOrNull()?.type?.resolve()
                 if (elementType != null) {
                     val elementTypeName = elementType.declaration.simpleName.asString()
                     val qElementTypeName = "Q$elementTypeName"
                     val collectionType = when {
-                        propertyType.isListType() -> "ListPath"
-                        propertyType.isSetType() -> "SetPath"
-                        else -> "CollectionPath"
+                        propertyType.isListType() -> "List"
+                        propertyType.isSetType() -> "Set"
+                        else -> "Collection"
                     }
-                    sb.append("    public final $collectionType<$elementTypeName, $qElementTypeName> $propertyName = this.<$elementTypeName, $qElementTypeName>create$collectionType(\"$propertyName\", $elementTypeName.class, $qElementTypeName.class, PathInits.DIRECT2);\n\n")
+                    ctx.append("    public final ${collectionType}Path<$elementTypeName, $qElementTypeName> $propertyName = this.<$elementTypeName, $qElementTypeName>create$collectionType(\"$propertyName\", $elementTypeName.class, $qElementTypeName.class, PathInits.DIRECT2);\n")
                 }
+            }
+            // Basic types
+            propertyType.isBasicType() -> {
+                logger?.info("Property $propertyName is a basic type: ${propertyType.declaration.simpleName.asString()} (${propertyType.declaration.qualifiedName?.asString()}) - adding as a simple path")
+                val pathType = getPathTypeForBasicType(propertyType)
+                ctx.append("    public final $pathType $propertyName = createSimple(\"$propertyName\", ${propertyType.declaration.qualifiedName?.asString()}.class);\n")
             }
             // Entity references
             propertyTypeDeclaration is KSClassDeclaration && propertyTypeDeclaration.annotations.any { it.shortName.asString() == "Entity" } -> {
                 val referencedEntityName = propertyTypeDeclaration.simpleName.asString()
                 val qReferencedEntityName = "Q$referencedEntityName"
-                sb.append("    public final $qReferencedEntityName $propertyName;\n\n")
+                ctx.append("    public final $qReferencedEntityName $propertyName;\n")
+                ctx.singularReferences.add(propertyName to propertyType)
             }
             // Other types
             else -> {
-                // Handle as simple path
-                sb.append("    public final SimplePath<${propertyTypeDeclaration.simpleName.asString()}> $propertyName = createSimple(\"$propertyName\", ${propertyTypeDeclaration.simpleName.asString()}.class);\n\n")
+                // Handle as a simple path
+                ctx.append("    public final SimplePath<${propertyTypeDeclaration.simpleName.asString()}> $propertyName = createSimple(\"$propertyName\", ${propertyTypeDeclaration.simpleName.asString()}.class);\n")
             }
         }
     }
 
-    private fun generateConstructors(sb: StringBuilder, className: String, qClassName: String) {
-        // Variable constructor
-        sb.append("    public $qClassName(String variable) {\n")
-        sb.append("        this($className.class, forVariable(variable), INITS);\n")
-        sb.append("    }\n\n")
+    private fun generateConstructors(ctx: ClassCtx, className: String, qClassName: String) {
+        val referenceInit = ctx.singularReferences.joinToString("\n") {
+            "this.${it.first} = inits.isInitialized(\"${it.first}\") ? new Q${it.second.declaration.simpleName.asString()}(forProperty(\"${it.first}\"), inits.get(\"${it.first}\")) : null;"
+        }
+        ctx.append(
+            """
+    public $qClassName(String variable) {
+        this($className.class, forVariable(variable), INITS);
+    }
 
-        // Path constructor
-        sb.append("    public $qClassName(Path<? extends $className> path) {\n")
-        sb.append("        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));\n")
-        sb.append("    }\n\n")
+    public $qClassName(Path<? extends $className> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
 
-        // Metadata constructor
-        sb.append("    public $qClassName(PathMetadata metadata) {\n")
-        sb.append("        this(metadata, PathInits.getFor(metadata, INITS));\n")
-        sb.append("    }\n\n")
+    public $qClassName(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
 
-        // Metadata with inits constructor
-        sb.append("    public $qClassName(PathMetadata metadata, PathInits inits) {\n")
-        sb.append("        this($className.class, metadata, inits);\n")
-        sb.append("    }\n\n")
+    public $qClassName(PathMetadata metadata, PathInits inits) {
+        this($className.class, metadata, inits);
+    }
 
-        // Full constructor
-        sb.append("    public $qClassName(Class<? extends $className> type, PathMetadata metadata, PathInits inits) {\n")
-        sb.append("        super(type, metadata, inits);\n")
-        sb.append("        // Initialize entity references here\n")
-        sb.append("    }\n\n")
+    public $qClassName(Class<? extends $className> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        $referenceInit
+    }
+        """
+        )
     }
 
     private fun KSType.isBasicType(): Boolean {
         val typeName = declaration.qualifiedName?.asString() ?: return false
         return typeName.startsWith("java.lang.") ||
+                typeName.startsWith("kotlin.") ||
                 typeName == "java.util.Date" ||
                 typeName == "java.time.LocalDate" ||
                 typeName == "java.time.LocalDateTime" ||
@@ -204,26 +228,36 @@ class QuerydslProcessor(
         val typeName = declaration.qualifiedName?.asString() ?: return false
         return typeName.startsWith("java.util.Collection") ||
                 typeName.startsWith("java.util.List") ||
-                typeName.startsWith("java.util.Set")
+                typeName.startsWith("java.util.Set") ||
+                typeName.startsWith("kotlin.collections") ||
+                typeName == "kotlin.Array"
     }
 
     private fun KSType.isListType(): Boolean {
         val typeName = declaration.qualifiedName?.asString() ?: return false
-        return typeName.startsWith("java.util.List")
+        return typeName.startsWith("java.util.List") ||
+                typeName == "kotlin.collections.List" ||
+                typeName == "kotlin.collections.MutableList"
     }
 
     private fun KSType.isSetType(): Boolean {
         val typeName = declaration.qualifiedName?.asString() ?: return false
-        return typeName.startsWith("java.util.Set")
+        return typeName.startsWith("java.util.Set") ||
+                typeName == "kotlin.collections.Set" ||
+                typeName == "kotlin.collections.MutableSet"
     }
 
     private fun getPathTypeForBasicType(type: KSType): String {
         val typeName = type.declaration.qualifiedName?.asString() ?: return "SimplePath<Object>"
         return when {
-            typeName == "java.lang.String" -> "StringPath"
-            typeName == "java.lang.Integer" || typeName == "int" -> "NumberPath<Integer>"
-            typeName == "java.lang.Long" || typeName == "long" -> "NumberPath<Long>"
-            typeName == "java.lang.Boolean" || typeName == "boolean" -> "BooleanPath"
+            typeName == "java.lang.String" || typeName == "kotlin.String" -> "StringPath"
+            typeName == "java.lang.Integer" || typeName == "int" || typeName == "kotlin.Int" -> "NumberPath<Integer>"
+            typeName == "java.lang.Long" || typeName == "long" || typeName == "kotlin.Long" -> "NumberPath<Long>"
+            typeName == "java.lang.Boolean" || typeName == "boolean" || typeName == "kotlin.Boolean" -> "BooleanPath"
+            typeName == "java.lang.Float" || typeName == "float" || typeName == "kotlin.Float" -> "NumberPath<Float>"
+            typeName == "java.lang.Double" || typeName == "double" || typeName == "kotlin.Double" -> "NumberPath<Double>"
+            typeName == "java.lang.Short" || typeName == "short" || typeName == "kotlin.Short" -> "NumberPath<Short>"
+            typeName == "java.lang.Byte" || typeName == "byte" || typeName == "kotlin.Byte" -> "NumberPath<Byte>"
             typeName == "java.util.Date" -> "DatePath<java.util.Date>"
             typeName == "java.time.LocalDate" -> "DatePath<java.time.LocalDate>"
             typeName == "java.time.LocalDateTime" -> "DateTimePath<java.time.LocalDateTime>"
@@ -238,6 +272,18 @@ class QuerydslProcessor(
         if (isEmpty() || !first().isUpperCase()) return this
         return first().lowercase() + substring(1)
     }
+
+    class ClassCtx(
+        val sb: java.lang.StringBuilder = StringBuilder(),
+        val fieldNames: MutableSet<String> = mutableSetOf(),
+        val singularReferences: MutableSet<Pair<String, KSType>> = mutableSetOf()
+    ) {
+        fun append(lines: String) {
+            sb.append(lines + "\n")
+        }
+        override fun toString(): String = sb.toString()
+    }
+
 }
 
 /**
